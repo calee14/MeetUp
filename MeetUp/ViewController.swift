64d9e0be8d8e139af449d8a7a647d7f06abb5c0d
CallmeH <helen.pei.qin@outlook.com>
2018-07-16 13:03:46 -0700

    Basic UI for welcome & setup
    Next: segues

--- MeetUp/ViewController.swift
+++ MeetUp/ViewController.swift
@@ -23,3 +23,33 @@ class ViewController: UIViewController {
 
 }
 
+class SetUpViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    @IBOutlet weak var picker: UIPickerView!
+    var pickerData: [String] = [String]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        pickerData = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+        
+    }
+}
+
+
