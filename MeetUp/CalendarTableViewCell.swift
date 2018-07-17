5153f7e750049cbabbf13ce7565cf6a061bbd8bb
calee14 <caplee1000@gmail.com>
2018-07-16 17:02:28 -0700

    added calendar storyboard

---
+++ MeetUp/CalendarTableViewCell.swift
@@ -0,0 +1,79 @@
+//
+//  CalendarTableViewCell.swift
+//  MeetUp
+//
+//  Created by Cappillen on 7/16/18.
+//  Copyright © 2018 Cappillen. All rights reserved.
+//
+
+import UIKit
+
+class CalendarTableViewCell: UITableViewCell {
+    
+    private let topTimeLabel = UILabel()
+    private let bottomTimeLabel = UILabel()
+    
+    private let separatorLine = UIView()
+    
+    var topTime: String = "" {
+        didSet {
+            topTimeLabel.text = topTime
+        }
+    }
+    var bottomTime: String = "" {
+        didSet {
+            bottomTimeLabel.text = bottomTime
+        }
+    }
+    
+    var selectedTime: Bool = false
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        selectionStyle = .none
+        
+        contentView.addSubview(topTimeLabel)
+        contentView.addSubview(bottomTimeLabel)
+        contentView.addSubview(separatorLine)
+        
+        
+        topTimeLabel.textColor = UIColor.gray
+        topTimeLabel.textAlignment = .right
+        bottomTimeLabel.textColor = UIColor.gray
+        bottomTimeLabel.textAlignment = .right
+        separatorLine.backgroundColor = UIColor.gray
+        
+        bottomTimeLabel.translatesAutoresizingMaskIntoConstraints = false
+        topTimeLabel.translatesAutoresizingMaskIntoConstraints = false
+        separatorLine.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            bottomTimeLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 0),
+            bottomTimeLabel.centerYAnchor.constraint(equalTo: self.bottomAnchor),
+            bottomTimeLabel.widthAnchor.constraint(equalToConstant: 50),
+            topTimeLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 0),
+            topTimeLabel.centerYAnchor.constraint(equalTo: self.topAnchor),
+            topTimeLabel.widthAnchor.constraint(equalToConstant: 50),
+            separatorLine.leftAnchor.constraint(equalTo: bottomTimeLabel.rightAnchor, constant: 8),
+            separatorLine.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            separatorLine.heightAnchor.constraint(equalToConstant: 1),
+            separatorLine.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 0),
+            ])
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MeetUp/CalendarViewController.swift
+++ MeetUp/CalendarViewController.swift
@@ -10,26 +10,71 @@ import UIKit
 
 class CalendarViewController: UIViewController {
 
+    @IBOutlet weak var amTableView: UITableView!
+    @IBOutlet weak var pmTableView: UITableView!
+    
+    var selectedCells: [Int] = [Int]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        amTableView.separatorStyle = .none
+        pmTableView.separatorStyle = .none
+        amTableView.isScrollEnabled = false
+        pmTableView.isScrollEnabled = false
+        amTableView.register(CalendarTableViewCell.self, forCellReuseIdentifier: "AMCalendarCell")
+        pmTableView.register(CalendarTableViewCell.self, forCellReuseIdentifier: "PMCalendarCell")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension CalendarViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 12
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = CalendarTableViewCell()
+        if tableView == amTableView {
+            print("amtable")
+            cell = tableView.dequeueReusableCell(withIdentifier: "AMCalendarCell", for: indexPath) as! CalendarTableViewCell
+        } else if tableView == pmTableView {
+            print("pmtable")
+            cell = tableView.dequeueReusableCell(withIdentifier: "PMCalendarCell", for: indexPath) as! CalendarTableViewCell
+        }
+        if selectedCells.contains(indexPath.row) {
+            cell.backgroundColor = .green
+        }
+        if indexPath.row > 0 {
+            cell.topTime = "\(indexPath.row):00"
+        } else {
+            cell.topTime = ""
+        }
+        cell.bottomTime = "\(indexPath.row + 1):00"
+        return cell
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let tableHeight = tableView.frame.size.height
+        return CGFloat(tableHeight / 13)
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.selectedCells.append(indexPath.row)
+        tableView.reloadData()
+    }
+    
+//    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+//        var cell = CalendarTableViewCell()
+//        if tableView == amTableView {
+////            print("amtable")
+//            cell = tableView.dequeueReusableCell(withIdentifier: "AMCalendarCell", for: indexPath) as! CalendarTableViewCell
+//        } else if tableView == pmTableView {
+////            print("pmtable")
+//            cell = tableView.dequeueReusableCell(withIdentifier: "PMCalendarCell", for: indexPath) as! CalendarTableViewCell
+//        }
+//        cell.backgroundColor = .clear
+////        print("selected \(cell.topTimeLabel.text)")
+//    }
 }
