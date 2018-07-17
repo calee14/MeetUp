0d246ce31227502f6c53cb92434e4946cbf31789
CallmeH <helen.pei.qin@outlook.com>
2018-07-17 16:06:52 -0700

    Added Date choice page (pull practice)

---
+++ MeetUp/DateViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DateViewController.swift
+//  MeetUp
+//
+//  Created by Pei Qin on 2018/7/17.
+//  Copyright © 2018 Cappillen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DateViewController: UIViewController {
+    
+    var testFromDurationPage = TestClass(NewTime: 0, NewMember: 0)
+    
+    @IBAction func toCalendar(_ sender: Any) {
+        let intializeCalendarView = UIStoryboard.initializeViewController(for: UIStoryboard.MeetSBType.testboard)//changeForCal
+        self.view.window?.rootViewController = intializeCalendarView
+        self.view.window?.makeKeyAndVisible()
+    }
+    
+//    var pickedDate =
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let identifier = segue.identifier else {return}
+        switch identifier {
+        case "toCalendar"://changeForCal
+            let destination = segue.destination as! TestViewController
+            destination.testFromDatePage.NewTime = testFromDurationPage.NewTime
+            destination.testFromDatePage.NewMember = testFromDurationPage.NewMember
+            destination.testFromDatePage.NewDate = testFromDurationPage.NewDate
+        default:
+            print("something went wrong")
+        }
+    }
+    
+}
