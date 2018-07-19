4f5a00a9d62a56e962c7bfeac5c993f0e5971e90
CallmeH <helen.pei.qin@outlook.com>
2018-07-19 15:31:39 -0700

    Manually merged Audrey's new user tutorial
    Changed files: main storyboard, SetupViewController, TutorialViewController, Assets
    
    Didn't touch the calendar stuff.

--- MeetUp/ViewControllers/SetupViewController.swift
+++ MeetUp/ViewControllers/SetupViewController.swift
@@ -44,7 +44,44 @@ class SetupViewController: UIViewController {
 //        self.view.window?.rootViewController = backToNew
 //        self.view.window?.makeKeyAndVisible()
     }
+    
+    @IBAction func unwindWithTutorial(_ segue: UIStoryboardSegue){
+        let initialViewController = UIStoryboard.initializeViewController(for: UIStoryboard.MeetSBType.main)
+        self.view.window?.rootViewController = initialViewController
+        self.view.window?.makeKeyAndVisible()
+    }
+}
 
+@IBDesignable extension UIButton {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
 }
 
 
